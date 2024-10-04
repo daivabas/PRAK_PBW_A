078ee418d314b03900b10672d2c7cec10b8fb705
daivabas <145968640+daivabas@users.noreply.github.com>
2024-10-04 11:30:17 +0700

    Add files via upload

---
+++ TUGAS5/mvc/views/home/index.php
@@ -0,0 +1,6 @@
+<link rel="stylesheet" type="text/css" href="<?= BASEURL; ?>assets/css/style_home_index.css">
+<h1>Selamat Datang di Aplikasi Penjualan Barang</h1>
+<p>Ini adalah halaman home.</p>
+<a href="<?= BASEURL; ?>index.php?url=login/index">Login</a>
+
+
---
+++ TUGAS5/mvc/views/login/index.php
@@ -0,0 +1,7 @@
+<form action="<?= BASEURL; ?>index.php?url=login/auth" method="POST">
+<h2>Form Login</h2>
+    <input type="text" name="username" placeholder="Username" required><br>
+    <input type="password" name="password" placeholder="Password" required><br>
+    <button type="submit">Login</button>
+</form>
+<link rel="stylesheet" type="text/css" href="/mvc/assets/css/style_login_index.css">
